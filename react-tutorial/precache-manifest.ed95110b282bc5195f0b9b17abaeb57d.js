87f22f7aa0aefb1515c453e65fc0a554d6e3a9d3
astridx <info@astrid-guenther.de>
2020-06-30 18:17:16 +0200

    react tut

---
+++ react-tutorial/precache-manifest.ed95110b282bc5195f0b9b17abaeb57d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4252b12fcf01ebdff1224107cf7d784",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a5546c8b5105f0fe6a6a",
+    "url": "/react-tutorial/static/css/main.6dcc3ae9.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5546c8b5105f0fe6a6a",
+    "url": "/react-tutorial/static/js/main.6d66bcce.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
