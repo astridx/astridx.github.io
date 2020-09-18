76de0b655420d7700dc64b72ada965d7f1f8716c
astridx <info@astrid-guenther.de>
2020-09-18 13:12:23 +0200

    homepage path

--- react-mapbox-example/service-worker.js
+++ react-mapbox-example/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63440617b39b2adf7c8bc37cbdf3fe3b.js"
+  "/react-mapbox-example/precache-manifest.6f69bc80f8b1e8aca8c2a6d38e0daf84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-mapbox-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
